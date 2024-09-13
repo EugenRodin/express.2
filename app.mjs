f9cf939256f5c48e246a674f69f1aaa9daf4288c
EugenRodin <gameover59630@gmail.com>
2024-09-13 12:36:59 +0200

    express.2Ð.1

--- app.mjs
+++ app.mjs
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url'
 
 const app = express()
 
-// Якщо потрібно використовувати __dirname в ES-модулі
+// Для визначення поточної директорії
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 

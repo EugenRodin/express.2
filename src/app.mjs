c680118bbbb2f1a82b710df284aecf7e0bef3d73
EugenRodin <gameover59630@gmail.com>
2024-10-06 23:51:35 +0200

    debug

--- src/app.mjs
+++ src/app.mjs
@@ -16,6 +16,7 @@ import CONFIG from './config.mjs'
 import { getUsers } from './services/userService.mjs'
 import { ObjectId } from 'mongodb'
 import chalk from 'chalk'
+import debug from 'debug'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -34,7 +35,7 @@ async function connect() {
   try { 
     // Встановлення з'єднання з MongoDB Atlas 
     await client.connect() 
-    console.log('Успішно підключено до MongoDB Atlas') 
+    debug('Успішно підключено до MongoDB Atlas') 
     
     // Отримання посилання на базу даних у Atlas 
     const db = client.db(dbName) 
@@ -43,13 +44,13 @@ async function connect() {
     const collections = await db.listCollections().toArray()
     if (!collections.some(col => col.name === 'test')) {
       await db.createCollection('test')
-      console.log('Колекцію test створено успішно')
+      debug('Колекцію test створено успішно')
     }
 
     // Перевірка, чи існує документ у колекції test
     const testDocument = await db.collection('test').findOne({ name: 'Test Document 1' })
     if (!testDocument) {
-      console.log('Документ не існує, додаємо нові документи')
+      debug('Документ не існує, додаємо нові документи')
 
       // Додавання документів до колекції
       await db.collection('test').insertMany([
@@ -59,19 +60,19 @@ async function connect() {
         { name: 'Test Document 4', age: 40 },
         { name: 'Test Document 5', age: 21 }
       ])
-      console.log('Документи додано успішно')
+      debug('Документи додано успішно')
     } else {
-      console.log('Документи вже існують')
+      debug('Документи вже існують')
     }
 
-    // Визначаємо критерій для вибору документа
+    // Визначаємо критерій для вибору документу
     const query = { name: 'Test Document' }
     // Визначаємо нові значення для оновлення
     const update = { $set: { name: 'test document updated' } }
 
     // Оновлюємо один документ, який відповідає критерію
     const result = await db.collection('test').updateOne(query, update)
-    console.log(`Оновлено ${result.modifiedCount} документ(ів)`)
+    debug(`Оновлено ${result.modifiedCount} документ(ів)`)
 
     // Перевірка, чи існують користувачі у колекції users
     const userCount = await db.collection('users').countDocuments()
@@ -79,17 +80,17 @@ async function connect() {
       // Додавання користувачів до колекції, якщо вони не існують
       const usersFromService = getUsers()
       await db.collection('users').insertMany(usersFromService)
-      console.log('Користувачів додано успішно')
+      debug('Користувачів додано успішно')
     } else {
-      console.log('Користувачі вже існують')
+      debug('Користувачі вже існують')
     }
 
-    // Визначаємо критерій для видалення документа
+    // Визначаємо критерій для видалення документу
     const deleteQuery = { email: 'jane@example.com' }
 
     // Видаляємо один документ, який відповідає критерію
     const deleteResult = await db.collection('users').deleteOne(deleteQuery)
-    console.log(`Видалено ${deleteResult.deletedCount} документ(ів)`)
+    debug(`Видалено ${deleteResult.deletedCount} документ(ів)`)
 
     // Встановлюємо projection для отримання лише імені та email кожного користувача
     const userQuery = {}
@@ -97,7 +98,7 @@ async function connect() {
 
     const usersCollection = db.collection('users')
     const usersList = await usersCollection.find(userQuery, { projection }).toArray()
-    console.log('Користувачі лише з іменем та email', usersList)
+    debug('Користувачі лише з іменем та email', usersList)
 
     // Сортування документів за віком у порядку зростання та обмеження до 3 документів
     const ascendingSortedDocuments = await db
@@ -106,7 +107,7 @@ async function connect() {
     .sort({ age: 1 })
     .limit(3)
     .toArray()
-      console.log(chalk.magentaBright('Top 3 documents sorted by age in ascending order:'), ascendingSortedDocuments)
+      debug(chalk.magentaBright('Top 3 documents sorted by age in ascending order:'), ascendingSortedDocuments)
 
     // Сортування документів за віком у порядку спадання та обмеження до 3 документів
     const descendingSortedDocuments = await db
@@ -115,7 +116,7 @@ async function connect() {
     .sort({ age: -1 })
     .limit(3)
     .toArray()
-      console.log(chalk.magentaBright('Top 3 documents sorted by age in descending order:'), descendingSortedDocuments)
+      debug(chalk.magentaBright('Top 3 documents sorted by age in descending order:'), descendingSortedDocuments)
   } catch (err) {
     console.error('Помилка при роботі з базою даних', err)
   }
@@ -348,7 +349,7 @@ app.get('/protected', ensureAuthenticated, (req, res) => {
 
 // Запуск сервера
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+  debug(`Server is running on http://localhost:${PORT}`)
 })
 
 export default app
